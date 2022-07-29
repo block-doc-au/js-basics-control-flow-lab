f3ba25868804381cf906d4baeed25b19561278f8
block-doc-au <neilc@cns-gc.com.au>
2022-07-29 14:08:33 +1000

    NC Completed Basic control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+    if (distance <= 400) return 'This one is on me!';
+    if (distance >2000 && distance <= 2500) return 'I will gladly take your thirty bucks.';
+    if (distance>2500) return 'No can do.';
+    
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+return (cityName == "NYC" ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipType){
   // Write your code here!
+  switch (tipType){
+    case 'generous':
+      return "Thank you so much.";
+      break;
+
+     case 'not as generous':
+        return "Thank you.";
+        break;
+      default:
+        return 'Bye.';
+  }
+
+
+  
 }
+
+//scuberGreetingForFeet(199);
+//scuberGreetingForFeet(2010);
+scuberGreetingForFeet(3000);
+
